3d2bd174a4f7dbf9dfdd4489c3b5faf78d6d4b27
Tresna Widiyaman <tresna.widiyaman@gmail.com>
2019-01-23 11:21:00 +0700

    pindah posisi php

--- register.php
+++ register.php
@@ -1,3 +1,17 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Register Pesbuk</title>
+
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body class="bg-light">
+
 <?php
 
 require_once("config.php");
@@ -34,18 +48,6 @@ if(isset($_POST['register'])){
 }
 
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register Pesbuk</title>
-
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-</head>
-<body class="bg-light">
 
 <div class="container mt-5">
     <div class="row">
